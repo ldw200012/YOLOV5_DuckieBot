12f784a7a0b7dadefd16cabed2ea87ba0c8e4897
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-04-26 16:45:43 +0200

    Asd

--- scripts/camera_saver.py
+++ scripts/camera_saver.py
@@ -1,11 +1,20 @@
 #!usr/bin/env python
 import rospy
+from PIL import Image
+import numpy as np
 from sensor_msgs.msg import CompressedImage
 
 
 def callback(data):
 	pub = rospy.Publisher('camera_input_check', CompressedImage, queue_size=100)
-	pub.publish(data)
+	pub.publish(data.shape)
+
+	# w, h = 512, 512
+	# data = np.zeros((h, w, 3), dtype=np.uint8)
+	# data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
+	# img = Image.fromarray(data, 'RGB')
+	# img.save('my.png')
+	# img.show()
 
 
 def camera_saver():
