caa7aab09a4ae5d01beff91f52c7503779c741ef
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-04-26 16:16:56 +0200

    camera_saver.py modified

---
+++ scripts/camera_saver.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python
+import rospy
+from sensor_msgs.msg import CompressedImage
+
+
+def callback(data):
+	pub = rospy.Publisher('camera_input_check', CompressedImage, queue_size=100)
+	pub.publish(data)
+
+
+def camera_saver():
+	rospy.init_node('camera_node', anonymous=True)
+	rate = rospy.Rate(50)
+
+	while not rospy.is_shutdowm():
+		sub = rospy.Subscriber("/JoudiDuck/camera_node/image/compressed", CompressedImage, callback)
+		rate.sleep()
+
+
+if __name__=='__main__':
+	try:
+		camera_saver()
+	except rospy.ROSInterruptException:
+		pass
