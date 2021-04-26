f1923ad8ccb441ffbceb742d20ed829b62a7285f
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-04-26 17:24:13 +0200

    asd

--- scripts/camera_saver.py
+++ scripts/camera_saver.py
@@ -7,27 +7,27 @@ from sensor_msgs.msg import CompressedImage
 
 def callback(data):
 	pub = rospy.Publisher('camera_input_check', CompressedImage, queue_size=10)
-	pub.publish(len(data))
 
-	data = np.array(data)
-	data = data.reshape((480, 640))
+	img_array = np.array(data.data)
+	img_array = img_array.reshape((480, 640))
 
-	rescaled = (255.0 / data.max() * (data - data.min())).astype(np.uint8)
+	rescaled = (255.0 / img_array.max() * (img_array - img_array.min())).astype(np.uint8)
 
 	# w, h = 480, 640
 	# data = np.zeros((h, w, 3), dtype=np.uint8)
 	# data[0:240, 0:320] = [255, 0, 0] # red patch in upper left
 	# img = Image.fromarray(data, 'RGB')
-	
+
 	img = Image.fromarray(rescaled)
-	img.save('my.png')
+	img_name = "duckie_"+str(data.header.seq)
+	img.save(img_name)
 
 
 def camera_saver():
 	rospy.init_node('camera_node', anonymous=True)
 	rate = rospy.Rate(50)
 
-	for i in range(1): # 500
+	for i in range(5): # 500
 		sub = rospy.Subscriber("/JoudiDuck/camera_node/image/compressed", CompressedImage, callback)
 		rate.sleep()
 
