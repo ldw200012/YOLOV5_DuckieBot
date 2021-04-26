7550118ab30e077561c9f253d8ca10c83cb1be02
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-04-26 16:38:47 +0200

    Asd

--- scripts/camera_saver.py
+++ scripts/camera_saver.py
@@ -7,22 +7,18 @@ def callback(data):
 	pub = rospy.Publisher('camera_input_check', CompressedImage, queue_size=100)
 	pub.publish(data)
 
-	image_count += 1
-
 
 def camera_saver():
 	rospy.init_node('camera_node', anonymous=True)
 	rate = rospy.Rate(50)
 
-	while image_count < 10: # 500
+	for img_count in range(10): # 500
 		sub = rospy.Subscriber("/JoudiDuck/camera_node/image/compressed", CompressedImage, callback)
 		rate.sleep()
 
 
 if __name__=='__main__':
 	try:
-		global image_count
-		image_count = 0
 		camera_saver()
 	except rospy.ROSInterruptException:
 		pass
