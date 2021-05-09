706411e919f714bc6e04ec990c4ad7fbfe64c47d
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 00:09:31 +0200

    Update detect.py

--- scripts/detect.py
+++ scripts/detect.py
@@ -10,7 +10,7 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
-from duckietown_yolov5.scripts.models.experimental import attempt_load
+from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
