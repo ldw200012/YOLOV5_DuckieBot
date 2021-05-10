4e97472218ad201466abc7af448d7bfb4f26e42b
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 21:52:17 +0200

    Update distance_detection.py

--- scripts/distance_detection.py
+++ scripts/distance_detection.py
@@ -38,53 +38,53 @@ def distance_to_camera(knownWidth, focalLength, perWidth):
 # In[4]:
 
 
-# initialize the known distance from the camera to the object, which
-# in this case is 24 inches
-KNOWN_DISTANCE = 2.0
+# # initialize the known distance from the camera to the object, which
+# # in this case is 24 inches
+# KNOWN_DISTANCE = 2.0
 
-# initialize the known object width, which in this case, the piece of
-# paper is 12 inches wide
-KNOWN_WIDTH = 11.0
+# # initialize the known object width, which in this case, the piece of
+# # paper is 12 inches wide
+# KNOWN_WIDTH = 11.0
 
-# initialize the list of images that we'll be using
-IMAGE_PATHS = ["Pictures/DuckieTown/Duck.jpg"]
-               #"Pictures/DuckieTown/image2.jpeg", "Pictures/DuckieTown/image3.jpeg"]
-
-
-# In[5]:
+# # initialize the list of images that we'll be using
+# IMAGE_PATHS = ["Pictures/DuckieTown/Duck.jpg"]
+#                #"Pictures/DuckieTown/image2.jpeg", "Pictures/DuckieTown/image3.jpeg"]
 
 
-# load the furst image that contains an object that is KNOWN TO BE 2 feet
-# from our camera, then find the paper marker in the image, and initialize
-# the focal length
-image = cv2.imread(IMAGE_PATHS[0])
-marker = find_marker(image)
-focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+# # In[5]:
 
 
-# In[6]:
+# # load the furst image that contains an object that is KNOWN TO BE 2 feet
+# # from our camera, then find the paper marker in the image, and initialize
+# # the focal length
+# image = cv2.imread(IMAGE_PATHS[0])
+# marker = find_marker(image)
+# focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
 
 
-#loop over the images
-for imagePath in IMAGE_PATHS:
-   # load the image, find the marker in the image, then compute the
-# distance to the marker from the camera
-   image = cv2.imread(imagePath)
-   marker = find_marker(image)
-   inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
-
-# draw a bounding box around the image and display it
-   box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
-   box = np.int0(box)
-   cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-   cv2.putText(image, "%.2fft" % (inches / 12),
-       (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-       2.0, (0, 255, 0), 3)
-   cv2.imshow("image", image)
-   cv2.waitKey(0)
+# # In[6]:
 
 
-# In[ ]:
+# #loop over the images
+# for imagePath in IMAGE_PATHS:
+#    # load the image, find the marker in the image, then compute the
+# # distance to the marker from the camera
+#    image = cv2.imread(imagePath)
+#    marker = find_marker(image)
+#    inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+# # draw a bounding box around the image and display it
+#    box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+#    box = np.int0(box)
+#    cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+#    cv2.putText(image, "%.2fft" % (inches / 12),
+#        (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+#        2.0, (0, 255, 0), 3)
+#    cv2.imshow("image", image)
+#    cv2.waitKey(0)
+
+
+# # In[ ]:
 
 
 
