7a8120e224d01c6994377e15787781e3c463ad27
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 12:59:49 +0200

    Update detect_and_publish.py

--- scripts/detect_and_publish.py
+++ scripts/detect_and_publish.py
@@ -12,7 +12,7 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
 from cv_bridge import CvBridge, CvBridgeError
 
 
-def callback(data):
+def callback(data, opt):
     pub = rospy.Publisher('/JoudiDuck/camera_node/image/detected', Image, queue_size=10)
     
     # Save rostopic images into directory
@@ -23,28 +23,6 @@ def callback(data):
     if os.path.exists(img_save_path):
         os.remove(img_save_path)
     cv2.imwrite(img_save_path, cv_image)
-    
-    # Detection from directory and save
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/yolov5s_results/weights/best.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='../content/test/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    print(opt)
-    check_requirements()
 
     with torch.no_grad():
         if opt.update:  # update all models (to fix SourceChangeWarning)
@@ -67,9 +45,31 @@ if __name__ == '__main__':
     
     rospy.init_node('duckietown_yolov5')   
     
+    # Detection from directory and save
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/yolov5s_results/weights/best.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='../content/test/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    opt = parser.parse_args()
+    print(opt)
+    check_requirements()
+    
     # Subscribe image from rostopic        
     while not rospy.is_shutdown():         
-        rospy.Subscriber("/JoudiDuck/camera_node/image/raw", Image, callback)
+        rospy.Subscriber("/JoudiDuck/camera_node/image/raw", Image, callback, opt)
 
         
         
