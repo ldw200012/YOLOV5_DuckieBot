345ff1f052f0020b1b5d661a3aae6f99c9e48ede
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 21:47:30 +0200

    Update detect.py

--- scripts/detect.py
+++ scripts/detect.py
@@ -1,4 +1,18 @@
+# import argparse
+# import time
+# from pathlib import Path
 
+# import cv2
+# import torch
+# import torch.backends.cudnn as cudnn
+# from numpy import random
+
+# from models.experimental import attempt_load
+# from utils.datasets import LoadStreams, LoadImages
+# from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+#     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+# from utils.plots import plot_one_box
+# from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
 def detect(opt, save_img=False):
