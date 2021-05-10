dc04b55936fd81c2fd1e5a50d1e4ee7d943d1178
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 12:11:47 +0200

    Update detect_and_publish.py

--- scripts/detect_and_publish.py
+++ scripts/detect_and_publish.py
@@ -20,9 +20,6 @@ def callback(data):
     cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
     
     img_save_path = "../content/test/images/frame.jpg"
-    if os.path.isfile(img_save_path):
-        os.remove(img_save_path)    
-        
     cv2.imwrite(img_save_path, cv_image)
     
     # Detection from directory and save
