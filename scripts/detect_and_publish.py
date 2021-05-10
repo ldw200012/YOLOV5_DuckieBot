b1877d43e9a7514fcfba84a513cedd16a4f9007c
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 21:46:57 +0200

    Update detect_and_publish.py

--- scripts/detect_and_publish.py
+++ scripts/detect_and_publish.py
@@ -12,6 +12,18 @@ from utils.general import check_img_size, check_requirements, check_imshow, non_
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from cv_bridge import CvBridge, CvBridgeError
 
+import argparse
+import time
+from pathlib import Path
+
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized
+
 
 # def callback(data, opt):
 #     pub = rospy.Publisher('/JoudiDuck/camera_node/image/detected', Image, queue_size=10)
