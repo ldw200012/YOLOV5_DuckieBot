29de4cfe592f573218ec73a543997998f00867e5
DONGWOOK LEE <dongwooklee1201@gmail.com>
2021-05-10 12:19:21 +0200

    Update detect.py

--- scripts/detect.py
+++ scripts/detect.py
@@ -119,27 +119,29 @@ def detect(opt, save_img=False):
             if view_img:
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
+        
+            return im0
 
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video'
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
+#             # Save results (image with detections)
+#             if save_img:
+#                 if dataset.mode == 'image':
+#                     cv2.imwrite(save_path, im0)
+#                 else:  # 'video'
+#                     if vid_path != save_path:  # new video
+#                         vid_path = save_path
+#                         if isinstance(vid_writer, cv2.VideoWriter):
+#                             vid_writer.release()  # release previous video writer
 
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                    vid_writer.write(im0)
+#                         fourcc = 'mp4v'  # output video codec
+#                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
+#                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+#                     vid_writer.write(im0)
 
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
+#     if save_txt or save_img:
+#         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+#         print(f"Results saved to {save_dir}{s}")
 
-    print(f'Done. ({time.time() - t0:.3f}s)')
-    return f"{save_dir}{s}"
+#     print(f'Done. ({time.time() - t0:.3f}s)')
+#     return f"{save_dir}{s}"
